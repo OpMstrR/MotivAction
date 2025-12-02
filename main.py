5b4192a285459bb6dc10af9fd0d972014dae7fb7
NikitaNizhynskyi <st9176596@stud.duikt.edu.ua>
2025-12-02 09:17:49 +0200

    Fix indentation and formatting in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    c=int(input("a=")
-    d=int(input("b=")
+    c=int(input("a="))
+    d=int(input("b="))
     print("Result:", add(c, d))
+
